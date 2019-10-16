71977bfb1120cfc7a8bce3dcf2fa2409ba5342cc
Paul Xiao <paul.xiao@axel.org>
2019-10-16 18:30:23 +0800

    add prod build

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 const isProd = process.env.NODE_ENV === 'production'
 const api = {
-    url: isProd ? 'https://lit-badlands-54425.herokuapp.com/' : 'localhost:8080'
+    url: isProd ? 'https://lit-badlands-54425.herokuapp.com' : 'localhost:8080'
 }
 
 export const $http = axios.create({
-    baseURL: isProd ? 'https://lit-badlands-54425.herokuapp.com/' : 'localhost:8080'
+    baseURL: isProd ? 'https://lit-badlands-54425.herokuapp.com' : 'localhost:8080'
 })
 
 export default api
